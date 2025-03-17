340cfd35a22ccb4eed663ba03caa4ddedeb1a928
Nivin Anil <nivin@kodnest.com>
2025-03-17 14:43:19 +0530

    Day 4 commit

---
+++ FrontEnd/src/CustomerHomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './assets/style.css';
+
+export default function CustomerHomePage() {
+    return (
+        <div>
+            <h1>This is Customer Page</h1>
+        </div>
+    );
+}
---
+++ FrontEnd/src/Routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegistrationPage from "./RegistrationPage";
+import CustomerHomePage from "./CustomerHomePage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="/customerhome" element={<CustomerHomePage />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
